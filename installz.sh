54facdee6427589c9ddd30bfe7583ca16a3a74f3
valetzx <45054400+valetzx@users.noreply.github.com>
2022-06-04 16:00:24 +0000

    commit

--- installz.sh
+++ installz.sh
@@ -1,1 +1,7 @@
 #00 Codespace 01
+curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-one -d
+sudo zerotier-cli join 565799d8f62169a2
+sudo zerotier-cli listnetworks
+
+wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh ed8484bec
